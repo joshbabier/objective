6fe6cee879e968d15a6ed1b7d7c1ade3fcae6f66
joshbabier <joshbabier@gmail.com>
2015-06-23 14:58:38 -0500

    Updated the models to be able to add new applicants via the admin.

--- objective_app/models.py
+++ objective_app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Job(models.Model):
     name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     @property
     def num_skills(self):
@@ -19,10 +21,12 @@ class Job(models.Model):
 
 class Applicant(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField(null=True)
-    website = models.CharField(max_length=255, null=True)
-    cover_letter = models.TextField(null=True)
+    email = models.EmailField(default='', blank=True)
+    website = models.CharField(max_length=255, default='', blank=True)
+    cover_letter = models.TextField(default='', blank=True)
     job = models.ForeignKey(Job, related_name='applicants')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     @property
     def num_skills(self):
@@ -34,6 +38,8 @@ class Applicant(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255)
     applicant = models.ForeignKey(Applicant, related_name='skills')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __unicode__(self):
         return self.name
